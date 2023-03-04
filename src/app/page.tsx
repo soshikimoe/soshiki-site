623bb85c3194fb55c911d0fb987d08488f6cbab7
lina <me@jackli.dev>
2023-03-03 19:14:50 -0700

    style: mobile styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ export default function Home() {
 	return (
 		<>
 			<div className="w-full flex flex-col items-center justify-center text-3xl font-semibold text-center">
-				<div className="text-primary flex flex-col items-center">
+				<div className="text-primary flex flex-col items-center px-5">
 					<Image alt="soshiki-logo" src="/logo.png" width={150} height={150} />
-					<h1 className="text-5xl">Soshiki</h1>
-					<h2 className="text-2xl mt-5 font-normal">FOSS manga, anime, and light novel all-in-one app</h2>
-					<div className="mt-4 flex gap-6">
+					<h1 className="text-4xl sm:text-5xl">Soshiki</h1>
+					<h2 className="text-xl sm:text-2xl mt-4 font-normal">
+						FOSS manga, anime, and light novel all-in-one app
+					</h2>
+					<div className="mt-4 flex flex-col sm:flex-row gap-2 sm:gap-6">
 						<Button button_link="https://github.com/soshikimoe" button_text="Discord" />
 						<Button button_link="https://github.com/soshikimoe" button_text="User Guide" />
 					</div>
