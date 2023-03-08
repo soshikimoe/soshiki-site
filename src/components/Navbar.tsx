f88165ac350b36e2972d5cfe4258c5ee4fe66750
lina <me@jackli.dev>
2023-03-08 09:30:29 -0700

    feat: add github to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,7 @@ import { Transition } from "@headlessui/react";
 
 const navigation = [
 	{ name: "Home", href: "/" },
+	{ name: "Github", href: "https://github.com/soshikimoe", external: true },
 	{ name: "Discord", href: "/discord", external: true },
 ];
 
