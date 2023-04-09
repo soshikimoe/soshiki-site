9de5b7c88a481f031ef2e6d24e193ae5d18784a3
lina <me@jackli.dev>
2023-04-09 01:29:28 -0700

    docs: installation

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -28,8 +28,8 @@ const HelpPage = () => {
 				</div>
 				<div className="sm:pl-72 w-full">
 					<div className="px-10 py-9 max-w-4xl mx-auto">
-						<h1 className="font-semibold text-3xl">{article.title}</h1>
-						<div className="mt-4">
+						<h1 className="font-semibold text-3xl text-primary">{article.title}</h1>
+						<div className="mt-6">
 							<article>
 								<MDXStyles>
 									<MDXContent />
--- src/components/MDXStyles.tsx
+++ src/components/MDXStyles.tsx
@@ -13,8 +13,10 @@ export function MDXStyles({ children }: MDXStylesProps) {
 				"text-primary relative",
 				"prose-h1:text-2xl prose-h1:font-medium prose-h1:mb-2",
 				"prose-h2:text-xl prose-h2:font-medium prose-h2:mb-2",
-				"prose-h3:text-lg prose-h3:font-medium",
-				"prose-a:text-[#b21677]"
+				"prose-h3:text-lg prose-h3:font-medium prose-h3:mb-2",
+				"prose-a:text-[#b21677]",
+				"prose-ol:list-decimal prose-ol:list-outside prose-ol:ml-10",
+				"prose-strong:font-medium"
 			)}
 		>
 			{children}
