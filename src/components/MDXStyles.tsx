1b185d3fa96cfc6172a18ddf9b0dabe3a2f873b1
lina <me@jackli.dev>
2023-04-11 10:37:58 -0700

    docs: add contibuting

--- src/components/MDXStyles.tsx
+++ src/components/MDXStyles.tsx
@@ -16,7 +16,8 @@ export function MDXStyles({ children }: MDXStylesProps) {
 				"prose-h3:text-lg prose-h3:font-medium prose-h3:mb-2",
 				"prose-a:text-[#b21677]",
 				"prose-ol:list-decimal prose-ol:list-outside prose-ol:ml-10",
-				"prose-strong:font-medium"
+				"prose-strong:font-medium",
+				"prose-ul:list-disc prose-ul:list-outside prose-ul:ml-10"
 			)}
 		>
 			{children}
